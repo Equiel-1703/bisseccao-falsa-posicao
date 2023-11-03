0e8ce8d1f6c358e1fe81262a3ca5eecac4269a2c
Henrique Rodrigues <henrique.grdr@inf.ufpel.edu.br>
2023-11-02 22:52:07 -0300

    Implementado falsa posição

--- lib/bisseccao_falsa_posicao.ex
+++ lib/bisseccao_falsa_posicao.ex
@@ -1,4 +1,6 @@
 defmodule BisseccaoFalsaPosicao do
+  @moduledoc false
+
   def bisseccao(f, a, b, e) do
     x = (a + b) / 2.0
 
@@ -16,6 +18,48 @@ defmodule BisseccaoFalsaPosicao do
       true ->
         a = x
         bisseccao(f, a, b, e)
+    end
+  end
+
+  def falsa_posicao(f, a, b, e) do
+    f_a = f.(a)
+    f_b = f.(b)
+
+    x = (a * f_b - b * f_a) / (f_b - f_a)
+    f_x = f.(x)
+
+    cond do
+      abs(f_x) <= e ->
+        x
+
+      f_x * f_a < 0 ->
+        b = x
+        falsa_posicao(f, a, b, e)
+
+      true ->
+        a = x
+        falsa_posicao(f, a, b, e)
+    end
+  end
+
+  def falsa_posicao_it(f, a, b, it) do
+    f_a = f.(a)
+    f_b = f.(b)
+
+    x = (a * f_b - b * f_a) / (f_b - f_a)
+    f_x = f.(x)
+
+    cond do
+      it - 1 < 0 ->
+        x
+
+      f_x * f_a < 0 ->
+        b = x
+        falsa_posicao_it(f, a, b, it - 1)
+
+      true ->
+        a = x
+        falsa_posicao_it(f, a, b, it - 1)
     end
   end
 end
